512da9e4ba32f93ea07015c13a775a551c840b0c
mrthinger <evanpierce@aim.com>
2013-09-05 14:13:26 -0400

    update version

--- mc/evan/lib/ModInfo.java
+++ mc/evan/lib/ModInfo.java
@@ -8,7 +8,7 @@ public class ModInfo {
 
 	public static final String ID = "evanmc";
 	public static final String NAME = "HackCraft";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1 Beta";
 	public static final String CTAB_NAME = "HackCraft";
 	public static final ItemStack CTAB_IMG = new ItemStack(Items.cpick);
 	
